b42bd2df961d0f92f39b74ffa7812fc53dc127c7
isBae <bis0212@naver.com>
2022-08-12 18:42:57 +0900

    작업중

--- src/components/DragDrop.js
+++ src/components/DragDrop.js
@@ -6,8 +6,11 @@ import ReactDragListView from "react-drag-listview";
 import update from "immutability-helper";
 
 import { FcFolder, FcFile } from "react-icons/fc";
+import { BsThreeDotsVertical } from "react-icons/bs";
 
 import { Resizable } from "react-resizable";
+
+import moment from "moment";
 
 import "./DragDrop.css";
 
@@ -16,9 +19,10 @@ const DraggableBodyRow = ({
   moveRow,
   className,
   style,
+
   ...restProps
 }) => {
-  console.log("DraggableBodyRow - restProps : ", restProps);
+  // console.log("DraggableBodyRow - restProps : ", restProps);
   const ref = useRef();
   const [{ isOver, dropClassName }, drop] = useDrop(
     () => ({
@@ -35,8 +39,13 @@ const DraggableBodyRow = ({
         };
       },
       drop: (item) => {
-        console.log("item.index : ", item.index, "Index : ", index);
-        // moveRow(item.index, index);
+        // console.log("item.index : ", item.index, "Index : ", index);
+        console.log(
+          restProps.children[index].props.record.type === "folder"
+            ? "Folder"
+            : "File"
+        );
+        moveRow(item.index, index);
       },
     }),
     [index]
@@ -60,13 +69,24 @@ const DraggableBodyRow = ({
       className={`${className} ${isOver ? dropClassName : ""}`}
       style={isOver ? { outline: `2px dotted red`, ...style } : { ...style }}
       {...restProps}
-    ></tr>
+    >
+      {/* {console.log("restProps : ", restProps)} */}
+    </tr>
   );
 };
 
 const ResizableTitle = (props) => {
-  const { onResize, onResizeStart, onResizeStop, width, title, ...restProps } =
-    props;
+  const {
+    onResize,
+    onResizeStart,
+    onResizeStop,
+    width,
+    title,
+    fixed,
+    ...restProps
+  } = props;
+
+  console.log("fixed : ", props);
 
   if (!width) {
     return <th {...restProps} />;
@@ -84,20 +104,28 @@ const ResizableTitle = (props) => {
           }}
         />
       }
-      onResize={onResize}
+      onResize={!fixed && onResize}
+      // onResize={onResize}
       onResizeStart={onResizeStart}
       onResizeStop={onResizeStop}
       draggableOpts={{ enableUserSelectHack: false }}
     >
       {/* {console.log("restProps : ", restProps)} */}
       <th {...restProps}>{/* <span>{title}</span> */}</th>
+      {/* {console.log("restProps : ", fixed)} */}
     </Resizable>
   );
 };
 
 const DragDrop = () => {
+  const [keyword, setKeyword] = useState("");
   const [selectedRows, setSelectedRows] = useState([]);
   const [isResizing, setIsResizing] = useState(false);
+  const [isMouseOver, setIsMouseOver] = useState();
+
+  useEffect(() => {
+    console.log("isMouseOver : ", isMouseOver);
+  }, [isMouseOver]);
 
   const [columns, setColumns] = useState([
     {
@@ -105,8 +133,18 @@ const DragDrop = () => {
       dataIndex: "type",
       key: "type",
       width: 150,
+      fixed: true,
       render: (text, record) => {
-        console.log("Text : ", text, "record :", record);
+        switch (record.type) {
+          case "folder":
+            return <FcFolder />;
+
+          case "file":
+            return <FcFile />;
+
+          default:
+            return;
+        }
       },
     },
     {
@@ -114,46 +152,74 @@ const DragDrop = () => {
       dataIndex: "name",
       key: "name",
       width: 200,
+      fixed: false,
       sorter: (a, b) => a.name.localeCompare(b.name),
+      render: (text, record) => {
+        return (
+          <>
+            <span onMouseEnter={() => setIsMouseOver(record.key)}>
+              {record.name}
+            </span>
+            <span
+              className={`item-detail ${record.key}`}
+              style={
+                parseInt(isMouseOver) === parseInt(record.key)
+                  ? { visibility: `visible` }
+                  : { visibility: `hidden` }
+              }
+            >
+              <BsThreeDotsVertical />
+            </span>
+          </>
+        );
+      },
     },
     {
-      title: "Age",
-      dataIndex: "age",
-      key: "age",
+      title: "Modified",
+      dataIndex: "modified",
+      key: "modified",
       width: 200,
-      sorter: (a, b) => a.age - b.age,
+      fixed: false,
+      sorter: (a, b) => a.modified - b.modified,
     },
     {
-      title: "Address",
-      dataIndex: "address",
-      key: "address",
+      title: "Size",
+      dataIndex: "size",
+      key: "size",
       width: 300,
-      sorter: (a, b) => a.address.localeCompare(b.address),
+      fixed: false,
+      sorter: (a, b) => a.size.localeCompare(b.size),
     },
   ]);
   const [data, setData] = useState([
     {
       key: "1",
       type: "folder",
-      name: "James",
-      age: 26,
-      address: "India",
+      name: "Test Folder",
+      modified: moment().format("YYYY-MM-DD"),
+      size: 0,
     },
     {
       key: "2",
       type: "file",
       name: "Tobe",
-      age: 33,
-      address: "Korea",
+      modified: moment().format("YYYY-MM-DD"),
+      size: 1821,
     },
     {
       key: "3",
       type: "file",
       name: "Ssi bal",
-      age: 18,
-      address: "Japan",
+      modified: moment().format("YYYY-MM-DD"),
+      size: 17421,
     },
   ]);
+
+  useEffect(() => {
+    const list = data.filter((item) => item.name.includes(keyword));
+
+    setData(list);
+  }, [keyword]);
 
   useEffect(() => {
     console.log("isResizing : ", isResizing);
@@ -201,6 +267,7 @@ const DragDrop = () => {
     onHeaderCell: (column) => ({
       width: column.width,
       title: column.title,
+      fixed: column.fixed,
       onResize: handleResize(index),
       onResizeStart: onResizeStart,
       onResizeStop: onResizeStop,
@@ -229,51 +296,60 @@ const DragDrop = () => {
   };
 
   return (
-    <div className="container mt-5">
-      <DndProvider backend={HTML5Backend}>
-        <ReactDragListView.DragColumn {...dragProps}>
-          <Table
-            showSorterTooltip={false}
-            rowSelection={{
-              type: "checkbox",
-              selectedRowKeys: selectedRows,
-              onChange: (selectedRowKeys, selectedRows) => {
-                setSelectedRows(selectedRowKeys);
-              },
-
-              getCheckboxProps: (record) => ({
-                disabled: record.name === "Disabled User",
-                // Column configuration not to be checked
-                name: record.name,
-              }),
-            }}
-            bordered
-            pagination={false}
-            columns={mergeColumns}
-            dataSource={data}
-            components={components}
-            onRow={(record, index) => {
-              return {
-                onClick: (e) => {
-                  setSelectedRows([String(index + 1)]);
+    <>
+      <div className="keyword">
+        <input
+          type="text"
+          value={keyword}
+          onChange={(e) => setKeyword(e.target.value)}
+        />
+      </div>
+      <div className="container mt-5">
+        <DndProvider backend={HTML5Backend}>
+          <ReactDragListView.DragColumn {...dragProps}>
+            <Table
+              showSorterTooltip={false}
+              rowSelection={{
+                type: "checkbox",
+                selectedRowKeys: selectedRows,
+                onChange: (selectedRowKeys, selectedRows) => {
+                  setSelectedRows(selectedRowKeys);
                 },
 
-                index,
-                moveRow,
-              };
-            }}
-            onHeaderRow={(columns, index) => {
-              return {
-                onContextMenu: (e) => {
-                  setIsResizing(false);
-                  console.log("된다!!!!!");
-                },
-              };
-            }}
-          />
-        </ReactDragListView.DragColumn>
-      </DndProvider>
-    </div>
+                getCheckboxProps: (record) => ({
+                  disabled: record.name === "Disabled User",
+                  // Column configuration not to be checked
+                  name: record.name,
+                }),
+              }}
+              bordered
+              pagination={false}
+              columns={mergeColumns}
+              dataSource={data}
+              components={components}
+              onRow={(record, index) => {
+                return {
+                  onClick: (e) => {
+                    setSelectedRows([String(index + 1)]);
+                  },
+
+                  index,
+                  moveRow,
+                };
+              }}
+              onHeaderRow={(columns, index) => {
+                return {
+                  onContextMenu: (e) => {
+                    setIsResizing(false);
+                    console.log("된다!!!!!");
+                  },
+                };
+              }}
+            />
+          </ReactDragListView.DragColumn>
+        </DndProvider>
+      </div>
+    </>
   );
 };
 
